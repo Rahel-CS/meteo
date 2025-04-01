e87673bc235629c44013dc8c8748177350d7d5e8
Rahel <rahelhabtamu68@gmail.com>
2025-04-01 09:27:38 -0800

    injected more weather data

--- index.js
+++ index.js
@@ -2,11 +2,33 @@ function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time = 1000);
 
-    cityElement.innerHtml = response.data.city;
+    cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML =  Math.round(temperature);   
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML =  `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed} km/hr`;
+    timeElement.innerHTML = formatDate(date);
 }
 
+function formatDate(date){
+
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["sunday","monday","tuesday","wednesday","thursday","friday", "saturday"];
+    let day = days[date.getDay()];
+    if (minutes < 10) {
+        minutes =`0${minutes}`;
+    }
+
+
+    return  `${day} ${hours}:${minutes}`;
+}
 function searchCity(city){
     let apiKey = "200cc43bt60312a3e9528fo8814ed375";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
