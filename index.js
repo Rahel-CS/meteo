a1a8c6db5a4ce24e57e03d30408cb78325a15cc2
Rahel <rahelhabtamu68@gmail.com>
2025-03-31 21:05:08 -0800

    added api key

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+function searchCity(city){
+    
+    let apiKey = "200cc43bt60312a3e9528fo8814ed375";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+}
+console.log(apiUrl);
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHtml = searchInput.Value;
+    searchCity(searchInput.value);
     }
 
 
